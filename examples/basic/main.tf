a1ee778b3dae9b4757668a7689b15a24db325c5e
Gergely Dányi <gergely.danyi@p0.dev>
2026-08-01 23:02:26 -0500

    Version 0.1.7 (#9)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,7 +7,7 @@ provider "helm" {
 
 module "oauthed_mcp" {
   source  = "p0-security/p0-oauthed-mcp/kubernetes"
-  version = "0.1.6"
+  version = "0.1.7"
 
   release_name     = var.release_name
   namespace        = var.namespace
